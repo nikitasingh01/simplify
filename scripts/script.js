29ddf8934528f02720913647a89dc490807fbc9a
Hitesh Vadher <hiteshvadher099@gmail.com>
2021-05-25 22:16:04 +0530

    date picker is added to new project section

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('#datepicker').datepicker({
+        format: "dd/mm/yyyy",
+        autoclose: true,
+        todayHighlight: true,
+        showOtherMonths: true,
+        selectOtherMonths: true,
+        changeMonth: true,
+        changeYear: true,
+        orientation: "button",
+        clearBtn: true,
+        todayBtn: "linked"
+    }); 
+});
